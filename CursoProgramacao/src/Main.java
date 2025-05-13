ce24efb20cc96d448fa08b224aa6e18a6ac075e2
Carlosrjhoe <carlosrjhoe@gmail.com>
2025-05-13 19:15:11 -0300

    REFACTOR: Testes de app

--- CursoProgramacao/src/Main.java
+++ CursoProgramacao/src/Main.java
@@ -4,25 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-         Locale.setDefault(Locale.US);
+        Locale.setDefault(Locale.US);
 
-         int idade;
-         double salario;
-         double altura;
-         char genero;
-         String nome;
+        int idade;
+        double salario;
+        double altura;
+        char genero;
+        String nome;
 
-         idade = 39;
-         salario = 5800.5;
-         genero = 'M';
-         nome = "carlos Junior";
-         altura = 1.81;
+        idade = 39;
+        salario = 5800.5;
+        genero = 'M';
+        nome = "carlos Junior";
+        altura = 1.81;
 
-         System.out.println("NOME = " + nome);
-         System.out.println("SALARIO = " + String.format("%.2f",  salario));
-         System.out.println("GENERO = " + genero);
-         System.out.println("IDADE = " + idade);
-         System.out.printf("ALTURA = " + String.format("%.2f", altura));
+        System.out.println("NOME = " + nome);
+        System.out.println("SALARIO = " + String.format("%.2f", salario));
+        System.out.println("GENERO = " + genero);
+        System.out.println("IDADE = " + idade);
+        System.out.printf("ALTURA = " + String.format("%.2f", altura));
 
     }
 }
