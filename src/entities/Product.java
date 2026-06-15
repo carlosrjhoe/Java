845153ae0f6ca1464b1275e581f4f60ee7a92a13
carlosrjhoe <carlosrjhoe@gmail.com>
2026-06-15 12:44:18 -0300

    FEAT: Resolvendo exercicios com atributos e metodos de classe

---
+++ src/entities/Product.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class Product {
+    public String name;
+    public int quantity;
+    public double price;
+
+    public double totalValueInStock(){
+        return quantity * price;
+    }
+
+    public void addProducts(int quantity){
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity){
+        this.quantity -= quantity;
+    }
+
+    public String toString(){
+        return name
+                + ", R$ " + String.format("%.2f", price) + ", "
+                + quantity
+                + " units, Total: R$ "
+                + String.format("%.2f", totalValueInStock());
+    }
+}
