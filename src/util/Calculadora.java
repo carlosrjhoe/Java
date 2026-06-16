7b0d2aa73335ad250549c2deb78f1eaf5716543f
carlosrjhoe <carlosrjhoe@gmail.com>
2026-06-16 20:57:09 -0300

    STYLE: Estudando membros estáticos PARTE 1

---
+++ src/util/Calculadora.java
@@ -0,0 +1,13 @@
+package util;
+
+public class Calculadora {
+    public final double PI = 3.14159;
+
+    public double valorCircuferencia(double raio) {
+        return 2.0 * PI * raio;
+    }
+
+    public double valorVolume(double raio) {
+        return 4.0 * PI * Math.pow(raio, 3) / 3.0;
+    }
+}
