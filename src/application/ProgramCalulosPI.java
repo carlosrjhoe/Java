f64b78805034f3f9085cbb45d08bd7d09868d76b
carlosrjhoe <carlosrjhoe@gmail.com>
2026-06-16 21:10:42 -0300

    STYLE: Estudando membros estáticos PARTE 2

--- src/application/ProgramCalulosPI.java
+++ src/application/ProgramCalulosPI.java
@@ -1,30 +1,36 @@
 package application;
 
-import util.Calculadora;
-
 import java.util.Locale;
 import java.util.Map;
 import java.util.Scanner;
 
 public class ProgramCalulosPI {
 
+    public static final double PI = 3.14159;
+
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Calculadora objCalc = new Calculadora();
-
         System.out.print("Digite o raio: ");
         double raio = sc.nextDouble();
 
-        double circunferencia = objCalc.valorCircuferencia(raio);
-        double volume = objCalc.valorVolume(raio);
+        double circunferencia = valorCircuferencia(raio);
+        double volume = valorVolume(raio);
 
         System.out.printf("Circunferencia: %.2f%n", circunferencia);
         System.out.printf("Volume: %.2f%n", volume);
-        System.out.printf("PI: %.2f%n", objCalc.PI);
+        System.out.printf("PI: %.2f%n", PI);
 
         sc.close();
+    }
+
+    public static double valorCircuferencia(double raio) {
+        return 2.0 * PI * raio;
+    }
+
+    public static double valorVolume(double raio) {
+        return 4.0 * PI * Math.pow(raio, 3) / 3.0;
     }
 }
